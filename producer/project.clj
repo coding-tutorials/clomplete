5038e65ba1647874d623daa11117524227dae328
Wagner Leonardi <leonardiwagner@gmail.com>
2019-10-20 11:25:06 -0300

    producer communication

--- producer/project.clj
+++ producer/project.clj
@@ -1,3 +1,5 @@
 (defproject producer "0.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/core.async "0.4.500"]
+                 [aleph "0.4.6"]]
   :main ^:skip-aot producer.core)
--- producer/src/producer/core.clj
+++ producer/src/producer/core.clj
@@ -1,7 +1,22 @@
 (ns producer.core
-  (:gen-class))
+  (:require [aleph.http :as http]
+            [clojure.core.async :as async]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def consumer-channel (async/chan))
+
+(defn read-response [something]
+  (println (str "result: " something)))
+
+(defn call-consumer []
+  (println (type (http/get "https://google.com"))))
+
+(defn run-worker []
+  (do
+    (dotimes [_ 3] 
+      (async/put! consumer-channel (call-consumer) read-response))
+    (Thread/sleep 5000)))
+
+
+(defn -main [& args]
+  (while true
+    (run-worker)))
