76ec7148f002afb5322c3e21d768700240cdac54
giseleml <giselemaraleo@gmail.com>
2019-10-23 21:01:45 -0300

    Creating and populating tables

--- producer/resources/migrations/20191023-1844-create-table-order-products.up.sql
+++ producer/resources/migrations/20191023-1844-create-table-order-products.up.sql
@@ -3,6 +3,6 @@ CREATE TABLE order_products
   id uuid NOT NULL PRIMARY KEY,
   order_id uuid NOT NULL,
   product_id uuid NOT NULL,
-  FOREIGN KEY (order_id) REFERENCES orders(id)
+  FOREIGN KEY (order_id) REFERENCES orders(id),
   FOREIGN KEY (product_id) REFERENCES products(id)
 )
--- producer/resources/migrations/20191023-2013-insert-table-order-products.up.sql
+++ producer/resources/migrations/20191023-2013-insert-table-order-products.up.sql
@@ -1,34 +1,41 @@
+/*
+id uuid NOT NULL PRIMARY KEY,
+order_id uuid NOT NULL,
+product_id uuid NOT NULL,
+*/
+
+
 INSERT INTO order_products VALUES
 (
   '841cf0f2-ff76-4717-8bc8-3bafa411d5c3',
   'de6df3eb-c071-4cd4-9775-b57cd1109764',
-  'd4887fe8-0e3c-410f-ad0b-e07a73905d22',
+  'a644508d-b9b7-47ac-8d6e-714bf2017313'
 );
 
 INSERT INTO order_products VALUES
 (
   '88f3d432-7b48-4030-9980-5299ed05e5a0',
   '67a30862-5149-400b-b0a4-083969368cf3',
-  'ec6c2553-24fa-4727-8d3e-773a4572e5d9',
+  '55dd61e8-6840-4ac5-87f1-e1e4b9c30f56'
 );
 
 INSERT INTO order_products VALUES
 (
   '44dafe7e-5b99-4f0b-a38f-e50a6f1861f1',
   '2e45984a-e1d7-42c3-b550-4fce2cc8bff6',
-  '0667c368-bba4-43e5-b33c-f38a7d59b573',
+  '530703b6-c800-440d-a651-b393d7accd02'
 );
 
 INSERT INTO order_products VALUES
 (
   '594681e9-5177-4352-b9e9-ca8c993b24a5',
   '4f7a52d3-7d46-40c1-aacb-f6a8af5fdcd5',
-  '7ae03fc8-ea61-4f15-af21-66a9dd57e3ad',
+  '809a5885-6494-44d5-b79a-bcc266092eb8'
 );
 
 INSERT INTO order_products VALUES
 (
   'a2b4fb2e-734b-45a6-a6b7-21a6e577727f',
   '2f994552-e690-4f0d-a75f-3bc77e8c8a89',
-  'a56228a9-b03a-4443-afde-b713f9354437',
+  '6019a4f4-2aa2-446c-bd18-9d10ae1a4403'
 );
