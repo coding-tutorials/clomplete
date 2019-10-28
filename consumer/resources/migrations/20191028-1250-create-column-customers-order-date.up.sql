7eb243e7b52c3f566142d236696f5fd776106c6f
Wagner Leonardi <leonardiwagner@gmail.com>
2019-10-28 13:16:10 -0300

    fix migration

--- consumer/resources/migrations/20191028-1250-create-column-customers-order-date.up.sql
+++ consumer/resources/migrations/20191028-1250-create-column-customers-order-date.up.sql
@@ -1,7 +1,5 @@
 DELETE FROM order_products;
 DELETE FROM orders;
-DELETE FROM customers;
-DELETE FROM products;
 
 ALTER TABLE orders
     ADD COLUMN order_date timestamp without time zone NOT NULL;
